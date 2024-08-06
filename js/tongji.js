676327f1da812cae98694659229c29bb95bd37fe
PesionWorker <2357394024@qq.com>
2024-08-06 18:02:47 +0800

    delete

--- js/tongji.js
+++
@@ -1,8 +0,0 @@
-
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?0a57497c90746b53f26c5dd7f4e187c9";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
